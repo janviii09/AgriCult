0e4f8bf122dd2303e70b68c03b8591fdb702f8f4
jaanayra <guptajanvi0912@gmial.com>
2025-01-19 02:27:33 +0530

    adding

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text className='text-3xl font-bold text-red-600'>Index</Text>
+    </View>
+  );
+}
